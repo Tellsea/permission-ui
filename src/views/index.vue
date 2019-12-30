e40cde23c7bb585787ee53b9c820911039effe3b
tellsea <3210054449@qq.com>
2019-12-30 16:47:38 +0800

    index 细节优化

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,47 +1,58 @@
 <template>
     <div id="app">
         <el-container>
-            <el-header>
-                <el-menu class="el-menu-demo" mode="horizontal">
-                    <el-menu-item>
-                        <el-radio-group v-model="isCollapse">
-                            <el-radio-button :label="false">展开</el-radio-button>
-                            <el-radio-button :label="true">收起</el-radio-button>
-                        </el-radio-group>
-                    </el-menu-item>
-                    <el-menu-item index="1"><h1>权限管理系统 Templete</h1></el-menu-item>
-                    <el-dropdown size="medium" trigger="click">
+            <el-aside width="200" style="background-color: #545c64;">
+                <el-menu style="border-right: none;" class="el-menu-vertical-demo"
+                         :collapse="isCollapse"
+                         background-color="#545c64"
+                         text-color="#fff"
+                         active-text-color="#409EFF">
+                    <el-submenu v-for="(item, i) in menuList" :index="i + ''" :key="i">
+                        <template slot="title">
+                            <i :class="item.icon"></i>
+                            <span slot="title">{{item.name}}</span>
+                        </template>
+                        <el-menu-item v-for="(child, j) in item.children" :index="i + '-' + j" :key="j + '10000'"
+                                      @click="toPage(child.url)">{{child.name}}
+                        </el-menu-item>
+                    </el-submenu>
+                </el-menu>
+            </el-aside>
+            <el-container>
+                <el-header style="padding: 0;">
+                    <el-menu class="el-menu-demo" mode="horizontal">
+                        <el-menu-item>
+                            <i class="el-icon-s-fold" @click="changeSideStatus"></i>
+                        </el-menu-item>
+                        <el-menu-item>
+                            <span style="font-size: 18px;">权限管理系统</span>
+                        </el-menu-item>
+                        <el-dropdown size="medium" trigger="click">
                           <span class="el-dropdown-link">
                             当前账户：{{userInfo.userName}}<i class="el-icon-arrow-down el-icon--right"></i>
                           </span>
-                        <el-dropdown-menu slot="dropdown">
-                            <el-dropdown-item icon="el-icon-user" @click.native="toPage('/userInfo/myself')">个人中心
-                            </el-dropdown-item>
-                            <el-dropdown-item icon="el-icon-close" @click.native="logout">退出登录</el-dropdown-item>
-                        </el-dropdown-menu>
-                    </el-dropdown>
-                    <el-avatar shape="square" style="float: right; margin-top: 10px;"
-                               :src="userInfo.avatar">
-                    </el-avatar>
-                </el-menu>
-            </el-header>
-            <el-container>
-                <el-aside width="200">
-                    <el-menu style="border-right: none;" class="el-menu-vertical-demo"
-                             :collapse="isCollapse">
-                        <el-submenu v-for="(item, i) in menuList" :index="i + ''" :key="i">
-                            <template slot="title">
-                                <i :class="item.icon"></i>
-                                <span slot="title">{{item.name}}</span>
-                            </template>
-                            <el-menu-item v-for="(child, j) in item.children" :index="i + '-' + j" :key="j + '10000'"
-                                          @click="toPage(child.url)">{{child.name}}
-                            </el-menu-item>
-                        </el-submenu>
+                            <el-dropdown-menu slot="dropdown">
+                                <el-dropdown-item icon="el-icon-user" @click.native="toPage('/userInfo/myself')">个人中心
+                                </el-dropdown-item>
+                                <el-dropdown-item icon="el-icon-close" @click.native="logout">退出登录</el-dropdown-item>
+                            </el-dropdown-menu>
+                        </el-dropdown>
+                        <el-avatar shape="square" style="float: right; margin-top: 10px;"
+                                   :src="userInfo.avatar">
+                        </el-avatar>
                     </el-menu>
-                </el-aside>
-                <el-main>
-                    <router-view/>
+                </el-header>
+                <el-main style="padding: 0;">
+                    <el-tabs type="border-card" v-model="editableTabsValue" closable @edit="handleTabsEdit">
+                        <el-tab-pane
+                                :key="item.name"
+                                v-for="(item) in editableTabs"
+                                :label="item.title"
+                                :name="item.name"
+                                style="background-color: #f2f2f2; min-height: calc(100vh - 101px); margin: -15px;">
+                            <router-view/>
+                        </el-tab-pane>
+                    </el-tabs>
                 </el-main>
             </el-container>
         </el-container>
@@ -53,13 +64,56 @@
         name: 'index',
         data() {
             return {
+                editableTabsValue: '2',
+                editableTabs: [{
+                    title: '文章管理',
+                    name: '1',
+                    content: 'Tab 1 content'
+                }, {
+                    title: '文章列表',
+                    name: '2',
+                    content: 'Tab 2 content'
+                }],
+                tabIndex: 2,
+
                 userInfo: this.$store.state.userInfo,
                 isCollapse: false,
-                changeSideIcon: 'el-icon-arrow-left',
                 menuList: this.$store.state.menuList
             }
         },
         methods: {
+            // 改变侧边栏状态
+            changeSideStatus() {
+                this.isCollapse = this.isCollapse == true ? false : true;
+            },
+            handleTabsEdit(targetName, action) {
+                if (action === 'add') {
+                    let newTabName = ++this.tabIndex + '';
+                    this.editableTabs.push({
+                        title: 'New Tab',
+                        name: newTabName,
+                        content: 'New Tab content'
+                    });
+                    this.editableTabsValue = newTabName;
+                }
+                if (action === 'remove') {
+                    let tabs = this.editableTabs;
+                    let activeName = this.editableTabsValue;
+                    if (activeName === targetName) {
+                        tabs.forEach((tab, index) => {
+                            if (tab.name === targetName) {
+                                let nextTab = tabs[index + 1] || tabs[index - 1];
+                                if (nextTab) {
+                                    activeName = nextTab.name;
+                                }
+                            }
+                        });
+                    }
+
+                    this.editableTabsValue = activeName;
+                    this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+                }
+            },
             toPage(url) {
                 this.$router.push({path: url});
             },
@@ -87,17 +141,8 @@
         min-height: 400px;
     }
 
-    .el-main {
-        background-color: #f2f2f2;
-        padding: 0;
-    }
-
     .el-menu--inline .el-menu-item {
         padding-left: 50px !important;
-    }
-
-    .el-tabs__content {
-        min-height: calc(100vh - 100px) !important;
     }
 
     /* 下拉菜单 */
